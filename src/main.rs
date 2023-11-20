ccb3d2e44e0ee3a387c297c9fd903ae4a6a8165a
jacko-png <jackharro282@gmail.com>
2023-11-20 21:23:43 +1100

    Stamps is only finding one item in iterator Messy code. Plan to refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,50 @@
-use std::fs::*;
+use std::fs;
 // use std::io;
 use std::io::prelude::*;
 use regex::Regex;
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> std::io::Result<()> {
     // Grabbing the paths from argv is scope creep
-    let stamps = match file_ctns("stamps.txt") {
-        Ok(ctns) => ctns,
-        Err(e) => panic!("{e}"),
-    };
+    let tracklist = file_ctns("tracklist.txt").unwrap();
+    let stamps = file_ctns("stamps.txt").unwrap();
     
-    let tracklist = match file_ctns("tracklist.txt") {
-        Ok(ctns) => ctns,
-        Err(e) => panic!("{e}"),
-    };
+    let mut titles: Vec<&str> = vec![];
+
+    let tracklist_re = Regex::new(
+    /*
+    */
+    r#"^\d+\t(.*?)\t"#
+    ).unwrap();
     
-    let mut titles = vec![];
-    let re = Regex::new(r#"^((?P<track_number>\d*)\t"*+(?P<track_name>.*?)"*+\t)"#).unwrap();
-    
-    for (_, [_, _, trk_title]) in re.captures_iter(&tracklist).map(|c| c.extract()) {
-        titles.push(trk_title);
+    // let mut i = 0;
+    for (_, [title]) in tracklist_re.captures_iter(&tracklist).map(|c| c.extract()) {
+        titles.push(title);
+        // println!("{}th :  '{}'", i, title);
+        // i+=1;
     }
     
-    println!("{:?}", titles);
+    // println!("{:?}", titles);
     
-    let re = Regex::new(r#"([\d.\s]*)"#).unwrap();
+    let stamps_re = Regex::new(
+    /*
+    prevent matches of trailing digits in label name
+        |   * used to produce zero-length matches
+        |       |  Trailing tab is excluded (lookahead assertions are not in this crate)
+        v       v  v                          */
+    r#"^([\d.\s]+)\t"#
+    ).unwrap();
     
+    // change to i=0 if i is declared earlier
     let mut i = 0;
-    for (_, [ stamp ]) in re.captures_iter(&stamps).map(|c| c.extract()) {
+    for (_, [stamp]) in stamps_re.captures_iter(&stamps).map(|c| c.extract()) {
         let title = titles.get(i);
         let title = match title {
             Some(s) => s,
-            None => panic!("out of bounds ig"),
+            None => panic!("There are more stamps than there are track titles."),
         };
         
-        let s = format!("{}{}", stamp, title);
-        println!("{}", s);
+        let s = format!("{stamp}\t{title}");
+        println!("s: {}", s);
         
         i+=1
     }
@@ -44,10 +53,11 @@ fn main() -> Result<(), std::io::Error> {
 }
 
 
-// filenames are hardcoded by design (I'll learn argv later)
+
+
 fn file_ctns(f: &str) -> Result<String, std::io::Error> {
-    let mut f = File::open(f)?;
+    let mut f = fs::File::open(f)?;
     let mut ctns = String::new();
     let _ = f.read_to_string(&mut ctns); // std::io::Read trait (prelude)
     Ok(ctns)
-} // by opening f in a function, the File will be killed and can't be accidentally modified for whatever horrific reason
+}
